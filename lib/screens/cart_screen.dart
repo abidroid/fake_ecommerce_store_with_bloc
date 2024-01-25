8284fafc826dfe9e28274bb4b2b5f1f0fe6741d2
Muhammad Abid <abid@baladiexpress.com>
2024-01-25 10:47:23 +0500

    screens added

---
+++ lib/screens/cart_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CartScreen extends StatelessWidget {
+  const CartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Cart')),
+      body: const Placeholder(),
+    );
+  }
+}
---
+++ lib/screens/category_list_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CategoryListScreen extends StatelessWidget {
+  const CategoryListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Categories'),
+      ),
+      body: const Placeholder(),
+    );
+  }
+}
